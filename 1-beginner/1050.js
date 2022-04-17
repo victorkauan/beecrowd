b93895717e6b4f1cbdab2135648be32431f84fe5
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2022-04-17 14:00:35 -0300

    feat: problem 1050

---
+++ 1-beginner/1050.js
@@ -0,0 +1,33 @@
+var input = require("fs").readFileSync("./dev/stdin", "utf8");
+var lines = input.split("\n");
+
+var ddd = parseInt(lines.shift());
+
+switch (ddd) {
+  case 11:
+    console.log("Sao Paulo");
+    break;
+  case 19:
+    console.log("Campinas");
+    break;
+  case 21:
+    console.log("Rio de Janeiro");
+    break;
+  case 27:
+    console.log("Vitoria");
+    break;
+  case 31:
+    console.log("Belo Horizonte");
+    break;
+  case 32:
+    console.log("Juiz de Fora");
+    break;
+  case 61:
+    console.log("Brasilia");
+    break;
+  case 71:
+    console.log("Salvador");
+    break;
+  default:
+    console.log("DDD nao cadastrado");
+}
