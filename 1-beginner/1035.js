b4e7148e1a4e15ccce860a7b654b778d94bd11f9
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2022-04-17 13:26:52 -0300

    feat: problem 1035

---
+++ 1-beginner/1035.js
@@ -0,0 +1,11 @@
+var input = require("fs").readFileSync("./dev/stdin", "utf8");
+var lines = input.split("\n");
+
+var [A, B, C, D] = lines
+  .shift()
+  .split(" ")
+  .map((value) => parseInt(value));
+
+if (B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 === 0)
+  console.log("Valores aceitos");
+else console.log("Valores nao aceitos");
